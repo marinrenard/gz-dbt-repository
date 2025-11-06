38867e6fe7998aea7e669cc7c59b8656629c6e60
Marin Renard <marinrenard@hotmail.fr>
2025-11-06 14:45:58 +0000

    commit margin per order

---
+++ models/int_orders_margin.sql
@@ -0,0 +1,10 @@
+select
+  orders_id,
+  date_date,
+  sum(revenue) as revenue,
+  sum(quantity) as quantity,
+  sum(quantity * purchase_price) as purchase_cost,
+  sum(revenue - (quantity * purchase_price)) as margin
+from {{ ref("int_sales_margin")}}
+group by orders_id, date_date
+order by orders_id desc
